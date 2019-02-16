f93176e7e65a82e0ee8060889432a46f22f84b41
Kieran Grant <kieran.thehacker.grant@gmail.com>
2019-02-16 13:40:10 +1000

    Add build script and optimise cl-gauth code

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+sbcl --noinform --lose-on-corruption --no-userinit --no-sysinit --noprint --disable-debugger \
+     --eval '(require "asdf")' \
+     --eval '(asdf:make "cl-gauth")' \
+     --eval "(save-lisp-and-die \"gauth\" :executable t :save-runtime-options t #+sb-core-compression :compression #+sb-core-compression t :toplevel #'cl-gauth::entrypoint)"
--- gauth.lisp
+++ gauth.lisp
@@ -1,11 +1,18 @@
 ;; License: MIT
 ;; Based on gauth at https://github.com/pcarrier/gauth
 
+(declaim (optimize (speed 3) (safety 1) (debug 0) (space 0) (compilation-speed 0)))
+
 (in-package :cl-gauth)
 
+(declaim (ftype (function nil (values (unsigned-byte 64) (unsigned-byte 64))) time-stamp))
 (defun time-stamp ()
-  (floor (- (get-universal-time) #.(encode-universal-time 0 0 0 1 1 1970 0)) 30))
+  (let ((current-time (- (the (unsigned-byte 32) (get-universal-time))
+			 (the (unsigned-byte 32) #.(encode-universal-time 0 0 0 1 1 1970 0)))))
+    (declare (type (unsigned-byte 32) current-time))
+    (truncate current-time 30)))
 
+(declaim (ftype (function (string) string) normalize-secret))
 (defun normalize-secret (secret)
   (let* ((no-padding (remove-if (lambda (x) (char= x #\Space)) (string-upcase secret)))
 	 (pad-length (- 8 (rem (length no-padding) 8))))
@@ -16,22 +23,29 @@
 	    (write-char #\= out)))
 	no-padding)))
 
+(declaim (ftype (function (string (unsigned-byte 32)) string) auth-code))
 (defun auth-code (secret time)
   (let ((key (cl-base32:base32-to-bytes secret))
 	(msg (make-array 8 :element-type '(unsigned-byte 8)))
-	hash offset trunc (number 0))
+	hash offset trunc)
+    (declare (type (simple-array (unsigned-byte 8)) key msg))
     ;; msg is time stored in Big-Endian format
     (dotimes (i 8) (setf (elt msg i) (ldb (byte 8 (* 8 (- 7 i))) time)))
     (setf hash
 	  (let ((hmac (ironclad:make-hmac key 'ironclad:sha1)))
 	    (ironclad:update-hmac hmac msg)
-	    (ironclad:hmac-digest hmac)))
+	    (the (simple-array (unsigned-byte 8)) (ironclad:hmac-digest hmac))))
     (setf offset (logand (elt hash 19) #xf))
     (setf trunc (subseq hash offset (+ offset 4)))
     (setf (elt trunc 0) (logand (elt trunc 0) #x7f))
     ;; now lets build up number
-    (dotimes (i 4) (setf (ldb (byte 8 (* 8 (- 3 i))) number) (elt trunc i)))
-    (format nil "~6,'0d" (mod number 1000000))))
+    (format nil "~6,'0d"
+	    (mod
+	     (logior (ash (elt trunc 0) 24)
+		     (ash (elt trunc 1) 16)
+		     (ash (elt trunc 2) 8)
+		     (ash (elt trunc 3) 0))
+	     1000000))))
 
 (defun main ()
   (if (not (or
@@ -39,22 +53,22 @@
 	    (uiop/filesystem:file-exists-p (merge-pathnames ".config/gauth.csv" (user-homedir-pathname)))))
       (error "Missing gauth config file!"))
   (let* ((file-content
-	 (if (uiop/filesystem:file-exists-p
-	      (merge-pathnames ".config/gauth.csv.aes256" (user-homedir-pathname)))
-	     (with-input-from-string (in
-				      (uiop/run-program:run-program 
-				       `("openssl" "enc" "-d" "-aes256" "-md" "sha256" "-in"
-						   ,(uiop/pathname:unix-namestring
-						     (merge-pathnames ".config/gauth.csv.aes256"
-								      (user-homedir-pathname))))
-				       :input :interactive
-				       :output '(:string :stripped t)
-				       :error-output t))
-	       (loop for line = (read-line in nil nil) until (null line) collecting line))
-	     (with-open-file (in (merge-pathnames ".config/gauth.csv" (user-homedir-pathname))
-				 :direction :input)
-	       (loop for line = (read-line in nil nil) until (null line) collecting line))))
-	(max-length (max 10 (loop for item in file-content maximizing (length (elt (split-sequence:split-sequence #\: item) 0))))))
+	   (if (uiop/filesystem:file-exists-p
+		(merge-pathnames ".config/gauth.csv.aes256" (user-homedir-pathname)))
+	       (with-input-from-string (in
+					(uiop/run-program:run-program 
+					 `("openssl" "enc" "-d" "-aes256" "-md" "sha256" "-in"
+						     ,(uiop/pathname:unix-namestring
+						       (merge-pathnames ".config/gauth.csv.aes256"
+									(user-homedir-pathname))))
+					 :input :interactive
+					 :output '(:string :stripped t)
+					 :error-output t))
+		 (loop for line = (read-line in nil nil) until (null line) collecting line))
+	       (with-open-file (in (merge-pathnames ".config/gauth.csv" (user-homedir-pathname))
+				   :direction :input)
+		 (loop for line = (read-line in nil nil) until (null line) collecting line))))
+	 (max-length (max 10 (loop for item in file-content maximizing (length (elt (split-sequence:split-sequence #\: item) 0))))))
     (multiple-value-bind (current-time progress) (time-stamp)
       (format t (format nil "~~~dt prev   curr   next~~%" max-length))
       (loop for line in file-content do
@@ -68,3 +82,10 @@
 		     (auth-code secret (1+ current-time)))))
       (format t "[~29a]~%" (with-output-to-string (out) (dotimes (i progress) (write-char #\= out))))
       (terpri))))
+
+(defun entrypoint ()
+  (handler-case
+      (main)
+    (error (e)
+      (format *error-output* "An error occured: ~A~%" e)
+      (uiop:quit -1))))
